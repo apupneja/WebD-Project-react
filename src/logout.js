8814dc3238a1bb4918b18474002563f7abe3ff17
ananth243 <f20201797@goa.bits-pilani.ac.in>
2021-05-13 00:16:29 +0530

    Fixed logout

--- src/logout.js
+++ src/logout.js
@@ -1,7 +1,20 @@
+import {useEffect} from "react";
+import {useHistory} from "react-router-dom";
+
 const Logout = () => {
-    return ( 
-        <div>Logging out</div>
-     );
+  const history = useHistory();
+  useEffect(() => {
+    localStorage.removeItem("jwt");
+    fetch("http://localhost:8000/api/logout")
+      .then((res) => {
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        history.push("/admin");
+      });
+  }, []);
+  return <div></div>;
 }
  
 export default Logout;
