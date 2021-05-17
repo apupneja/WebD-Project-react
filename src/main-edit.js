1c311364ea0e221757b602dad531a77e976b40b1
ananth243 <raghavananth02@gmail.com>
2021-05-17 10:54:46 +0530

    Fixed the error in main edit

--- src/main-edit.js
+++ src/main-edit.js
@@ -1,61 +1,61 @@
-import { Button, Form, Message , Dropdown} from 'semantic-ui-react'
+import { Button, Form, Message, Dropdown } from "semantic-ui-react";
 import axios from "axios";
-import {useEffect, useState} from "react";
-import { useHistory } from 'react-router';
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import React from "react";
 
-let categoryoptions=[];
+let categoryOptions = [], inventory;
 
 const MainEdit = () => {
-  const [inventory, setInventory]=useState([]);
-  const [categoryOptions, setCategoryOptions]= useState([]);
-  const history=useHistory();
+  const [categoryOptions, setCategoryOptions] = useState([]);
+  const history = useHistory();
   useEffect(() => {
-    axios.post("http://localhost:8000/api/inventory", {
+    axios
+      .post("http://localhost:8000/api/inventory", {
         data: {
-            cookie: localStorage.getItem("jwt")
-          },
-          headers: {
-            "Content-Type": "application/json"
-          },
-    }).then(res=>{
-        const token=res.data.jwt;
-        if(token){
-            setInventory(res.data.inventory);
-            inventory.map(category=>{
-              const object={key: category.product,
-                text: category.product,
-                value: category.product,};
-              categoryoptions.push(object);
-            });
-            setCategoryOptions(categoryoptions);
+          cookie: localStorage.getItem("jwt"),
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        const token = res.data.jwt;
+        if (token) {
+          inventory = res.data.inventory;
+          inventory.map((category) => {
+            const object = {
+              key: category.product,
+              text: category.product,
+              value: category.product,
+            };
+            categoryOptions.push(object);
+          });
+          setCategoryOptions(categoryOptions);
+        } else {
+          console.log(res.data.message);
+          history.push("/");
         }
-        else{
-            console.log(res.data.message);
-            history.push("/");
-        }
-    })
-}, []);
-    return ( 
-        <div className="edit">
-            <Form error>
-            <Form.Input label='Category' placeholder='Enter the type of product'>
-                <Dropdown
-                        placeholder='Category'
-                        fluid
-                        selection
-                        options={categoryOptions}
-                />
-                <Form.Input label='Item' placeholder='Laptop' />
-                <Form.Input label='Quantity' placeholder='3' />
-                </Form.Input>
-                <Form.Input label='Location' placeholder='Location' />
-                <Button>Submit</Button>
-            </Form>
+      });
+  }, []);
+  return (
+    <div className="edit">
+      <Form error>
+        <Form.Input label="Category">
+          <Dropdown
+            placeholder="Category"
+            fluid
+            selection
+            options={categoryOptions}
+          />
+          <Form.Input label="Item" placeholder="Laptop" />
+          <Form.Input label="Quantity" placeholder="3" />
+        </Form.Input>
+        <Form.Input label="Location" placeholder="Location" />
+        <Button>Submit</Button>
+      </Form>
+    </div>
+  );
+};
 
-        </div>
-    
-    );
-}
- 
 export default MainEdit;
