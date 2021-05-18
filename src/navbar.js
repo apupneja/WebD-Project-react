46b1c0b12156a9292b6bf82b876248a2e434a09f
oak2905 <f20201172@goa.bits-pilani.ac.in>
2021-05-18 19:53:10 +0530

    Added sidebar

--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,9 @@
 // A basic navbar to add future links if needed
 // with the name of the top.
 import { Link} from 'react-router-dom';
-import React from 'react';
+import React,{useState} from 'react';
 import { useMediaQuery } from 'react-responsive';
+import {Icon} from 'semantic-ui-react';
 // Also, since we are keeping the app simple, All 
 // the css we need is in the index.css
 
@@ -11,12 +12,17 @@ const Navbar = () => {
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-device-width: 1145px)'
   })
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
     return ( 
       <div>
       {isTabletOrMobile && (
       <div>
+        
         <nav className="navbar">
-            <div className="name">
+        <Icon inverted name='bars' onClick={showSidebar}/>
+          {sidebar && (
+              <div className="name">
                 <Link to="./admin" className="link" >INVENTORY</Link>
                 <br></br>
                 <Link to="./edit" className="link" >EDIT INVENTORY</Link>
@@ -26,20 +32,22 @@ const Navbar = () => {
                 <Link to="./download" className="link">DOWNLOAD INVENTORY</Link>
                 <br></br>
                 <Link to="./logout" className="link">LOGOUT</Link>
-            </div>
+            </div>)}
         </nav>
       </div>
       )}
       {isDesktopOrLaptop && (
       <div>
         <nav className="navbar">
+        <Icon inverted name='bars' onClick={showSidebar}/>
+        {sidebar && (
             <div className="name">
                 <Link to="./admin" className="link" >INVENTORY</Link>
                 <Link to="./edit" className="link" >EDIT INVENTORY</Link>
                 <Link to="./signup" className="link" >CREATE NEW ADMIN</Link>
                 <Link to="./download" className="link">DOWNLOAD INVENTORY</Link>
                 <Link to="./logout" className="link">LOGOUT</Link>
-            </div>
+            </div>)}
         </nav>
       </div>
       )}
