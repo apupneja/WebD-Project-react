a5cf2347a6d00c87cfdfa716253a5d8b928a0b36
apupneja <apupneja2002@gmail.com>
2021-05-25 23:49:32 +0530

    Category search working

--- client/src/main.js
+++ client/src/main.js
@@ -2,46 +2,66 @@ import Component from "./component";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useHistory } from "react-router";
-
+ 
 // import ComponentToPrint from './ComponentToPrint';
-
+ 
 const capitalize = (s) => {
   if (typeof s !== "string") return "";
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
-
+ 
 const slice = (id) => {
   return id.slice(18, 24);
 };
-
+ 
+let datalist=[];
+ 
 const Main = () => {
-  const [inventory, setInventory] = useState([]);
+  const column=["details","_id"];
+  const [data, setData]=useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const history = useHistory();
-  const [error, setError] = useState("");
-
   const [message, setMessage] = useState(localStorage.getItem("message"));
+ 
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/inventory", {
+        headers: {
+          "Authorization": localStorage.getItem("jwt"),
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        datalist=res.data.inventory;
+        setData(datalist);
+      })
+      .catch((err) => {
+        history.push("/");
+      });
+  }, []);
+ 
   setTimeout(() => {
     localStorage.removeItem("message");
     setMessage("");
   }, 5000);
-
-  useEffect(()=>{
-    axios
-    .get("http://localhost:8000/api/inventory", {
-      headers: {
-        "Authorization": localStorage.getItem("jwt"),
-        "Content-Type": "application/json",
-      }
-    })
-    .then((res) => {
-      setInventory(res.data.inventory);
-    })
-    .catch((err) => {
-      history.push("/");
-    });
-
-  },[]);
-
+ 
+  function Filter(e){
+      setSearchTerm(e.target.value);
+      Filterdata(e.target.value);
+  }
+ 
+  function Filterdata(value){
+    const lcValue = value.toLowerCase();
+    if(lcValue===" "){
+        setData(datalist);
+    }
+    else{
+        const filteredData= datalist.filter(item=>{
+            return Object.keys(item).some(key=>column.includes(key)?false: item[key].toLowerCase().includes(lcValue))
+        })
+        setData(filteredData)
+    }
+  }
   return (
     <div>
       <h1>{message}</h1>
@@ -49,10 +69,22 @@ const Main = () => {
         <span className="headingText">Database</span>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <br></br>
+        <div className="search">
+          <input
+            type="text"
+            value={searchTerm}
+            placeholder="search"
+            onChange={(e) => {
+              Filter(e);
+            }}
+          ></input>
+          {data.length === 0 && <span>No records found to display!</span>}
+        </div>
+        <h1>{searchTerm}</h1>
         <div>
-          <div>
-            {inventory.map((product) => (
-              <div>
+          <div className="filter">
+            {data.map((product) => (
+              <div id={product._id}>
                 <br></br>
                 <h1 key={product._id}>{capitalize(product.product)}</h1>
                 <ul>
@@ -77,5 +109,5 @@ const Main = () => {
     </div>
   );
 };
-
+ 
 export default Main;
